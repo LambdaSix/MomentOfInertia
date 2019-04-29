88e909a9b7464209e64a666a3d100117f9a12c86
Nicola <LambdaSix@users.noreply.github.com>
2019-04-29 15:38:28 +0100

    Rename MoIInfo to MomentOfInertiaModule to fit with KSP naming

---
+++ MomentOfIntertia/MomentOfInertiaModule.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace MomentOfIntertia
+{
+    public class MomentOfInertiaModule : PartModule
+    {
+        [KSPField(guiActive = true, guiName = "Moment of Inertia", guiUnits = "kg-m^2")]
+        public Vector3 MomentOfInertia;
+
+        public Vector3 CalculateMomentOfInertia(Vessel v)
+        {
+            var inertiaTensor = InertiaTensor.Compute(v);
+            return inertiaTensor.Trace();
+        }
+
+        private int ticks = 0;
+
+        public override void OnFixedUpdate()
+        {
+            if (ticks > 100)
+            {
+                //Update I
+                var I = CalculateMomentOfInertia(vessel);
+                MomentOfInertia = I;
+                ticks = 0;
+            }
+            else
+            {
+                ticks++;
+            }
+        }
+    }
+}
